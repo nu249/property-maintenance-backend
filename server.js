c4e82d4730a77fced63b8f29f236256eac6d4ce1
Maryam <maryiammahdo2001@gmail.com>
2026-09-22 23:56:22 +0300

    Add CORS support

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const connectDB = require("./config/db");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 const userRoutes = require("./routes/userRoutes");
 const buildingRoutes = require("./routes/buildingRoutes");
 const apartmentRoutes = require("./routes/apartmentRoutes");
